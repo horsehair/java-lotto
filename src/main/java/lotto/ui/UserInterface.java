3e7412834e330cbb1e9207b1267ea095e728ab7e
Woojin Lee <dhkep03@gmail.com>
2022-11-12 22:42:49 +0900

    feat(UserInterface): 당첨 번호 입력 받기

--- src/main/java/lotto/ui/UserInterface.java
+++ src/main/java/lotto/ui/UserInterface.java
@@ -1,10 +1,14 @@
 package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInterface {
     private static final String ANNOUNCEMENT_INPUT_MONEY = "구입금액을 입력해 주세요.";
+    private static final String ANNOUNCEMENT_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public static int inputMoney() {
         System.out.println(ANNOUNCEMENT_INPUT_MONEY);
@@ -14,5 +18,15 @@ public class UserInterface {
 
     public static void printNumbers(List<Integer> numbers) {
         System.out.println(numbers);
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println(ANNOUNCEMENT_INPUT_WINNING_NUMBERS);
+        String inputNumbersRaw = Console.readLine();
+        List<Integer> inputWinningNumbers = new ArrayList<>(Arrays.asList(inputNumbersRaw.split(","))
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
+        return inputWinningNumbers;
     }
 }
