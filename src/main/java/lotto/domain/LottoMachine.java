e145d2423a9400bb059b5fb95967a93b2d8ab0d8
Woojin Lee <dhkep03@gmail.com>
2022-11-12 22:08:56 +0900

    feat(LottoMachine): 구매 개수 세기

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,9 +3,16 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoMachine {
+    private static final int AMOUNT_OF_LOTTO = 1_000;
+
     public static List<Lotto> publish(int inputMoney) {
         int countOfLottos = calculateCount(inputMoney);
         List<Lotto> lottos = create(countOfLottos);
         return lottos;
+    }
+
+    private static int calculateCount(int inputMoney) {
+        int count = inputMoney / AMOUNT_OF_LOTTO;
+        return count;
     }
 }
